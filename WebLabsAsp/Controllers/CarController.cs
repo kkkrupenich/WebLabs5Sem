2c515b7a092ab1861885246bc9c6b1a89db93d7d
Egor Krupenich <krupenich.egor@gmail.com>
2022-10-07 13:04:39 +0300

    lab05 #1

--- WebLabsAsp/Controllers/CarController.cs
+++ WebLabsAsp/Controllers/CarController.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components.Forms;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
@@ -13,11 +14,15 @@ namespace WebLabsAsp.Controllers
     public class CarController : Controller
     {
         private readonly ApplicationDbContext _context;
+        private readonly IWebHostEnvironment _hostEnvironment;
 
-        public CarController(ApplicationDbContext context)
+        public CarController(ApplicationDbContext context, IWebHostEnvironment hostEnvironment)
         {
             _context = context;
+            _hostEnvironment = hostEnvironment;
         }
+        
+        [BindProperty] public InputModel Input { get; set; }
 
         // GET: Car
         public async Task<IActionResult> Index()
@@ -48,6 +53,7 @@ namespace WebLabsAsp.Controllers
         // GET: Car/Create
         public IActionResult Create()
         {
+            ViewData["groups"] = _context.CarGroups.ToList();
             return View();
         }
 
@@ -56,11 +62,27 @@ namespace WebLabsAsp.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,Brand,Description,Price,Image")] Car car)
+        public async Task<IActionResult> Create(
+            [Bind("Id,Brand,Description,Price,Image, CarGroup")] Car car)
         {
+            
             if (ModelState.IsValid)
             {
                 car.Id = Guid.NewGuid();
+                if (Input.ImageUpload != null)
+                {
+                    using (var stream = new FileStream(Path.Combine(_hostEnvironment.WebRootPath, "cars",
+                               car.Id.ToString("N")), FileMode.Create))
+                    {
+                        await Input.ImageUpload.FormFile.CopyToAsync(stream);
+                    }
+
+                    car.Image = car.Id.ToString("N") + Input.ImageUpload.FormFile.FileName.Split('.')[1];
+                }
+                
+                car.Group = _context.CarGroups.
+                    FirstOrDefaultAsync(m => m.CarGroupId == car.CarGroupId).Result.GroupName;
+
                 _context.Add(car);
                 await _context.SaveChangesAsync();
                 return RedirectToAction(nameof(Index));
@@ -159,6 +181,11 @@ namespace WebLabsAsp.Controllers
         private bool CarExists(Guid id)
         {
           return (_context.Cars?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+
+        public class InputModel
+        {
+            [BindProperty] public FileUpload ImageUpload { get; set; }
         }
     }
 }
--- WebLabsAsp/Entities/Cars.cs
+++ WebLabsAsp/Entities/Cars.cs
@@ -6,21 +6,28 @@ public class Car
 {
     public Guid Id { get; set; }
     
+    [Required(ErrorMessage = "{0} is required")]
     public string Brand { get; set; }
     
+    [Required(ErrorMessage = "{0} is required")]
     public string Description { get; set; }
     
+    [Required(ErrorMessage = "{0} is required")]
     public int Price { get; set; }
-    
     public string Image { get; set; }
     
+    public string Group { get; set; }
+    
+    public Guid CarGroupId { get; set; }
+    
+    [Required(ErrorMessage = "{0} is required")]
     [Display(Name="Category")]
-    public CarGroup Group { get; set; }
+    public virtual CarGroup CarGroup { get; set; }
 }
 
 public class CarGroup
 {
     public Guid CarGroupId { get; set; }
-    
+    [Required(ErrorMessage = "{0} is required")]
     public string GroupName { get; set; }
 }
