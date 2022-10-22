2628389b1238913d27e7fe07ecf6137149e7f44a
Egor Krupenich <krupenich.egor@gmail.com>
2022-10-22 18:07:13 +0300

    lab8 task3 done

---
+++ WebLabsAsp.Blazor/Client/Models/CounterModel.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebLabsAsp.Blazor.Client.Models;
+
+public class CounterModel
+{
+    [Required]
+    [Range(0, int.MaxValue, ErrorMessage = "Choose value from interval: [0 - 2147483647].")]
+    public int customCount { get; set; }
+}
---
+++ WebLabsAsp/Migrations/20221018133409_Cars.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebLabsAsp.Migrations
+{
+    public partial class Cars : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "CarGroups",
+                columns: table => new
+                {
+                    CarGroupId = table.Column<Guid>(type: "TEXT", nullable: false),
+                    GroupName = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CarGroups", x => x.CarGroupId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Cars",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    Brand = table.Column<string>(type: "TEXT", nullable: false),
+                    Description = table.Column<string>(type: "TEXT", nullable: false),
+                    Price = table.Column<int>(type: "INTEGER", nullable: false),
+                    Image = table.Column<string>(type: "TEXT", nullable: true),
+                    CarGroupId = table.Column<Guid>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cars", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Cars_CarGroups_CarGroupId",
+                        column: x => x.CarGroupId,
+                        principalTable: "CarGroups",
+                        principalColumn: "CarGroupId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Cars_CarGroupId",
+                table: "Cars",
+                column: "CarGroupId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Cars");
+
+            migrationBuilder.DropTable(
+                name: "CarGroups");
+        }
+    }
+}
